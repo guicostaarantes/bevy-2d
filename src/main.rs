1f9ad2a85eaa69e4a3931112775c6438dbf0a18c
Guilherme Arantes <gui@aula.ac.uk>
2023-10-18 08:21:25 -0300

    feat: improvements to movement and screen sizing

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,93 @@
 mod movable;
 
 use bevy::prelude::*;
+use bevy::render::camera::ScalingMode;
 use bevy::{core_pipeline::clear_color::ClearColorConfig, time::Stopwatch};
-use movable::{Controllable, Jumpable, Movable};
+use movable::{Animated, Controllable, Jumpable, Movable};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                fit_canvas_to_parent: true,
-                ..default()
-            }),
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        fit_canvas_to_parent: true,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
         .add_systems(Startup, setup)
         .add_systems(Update, move_movable_components)
         .add_systems(Update, move_controllable_characters)
+        .add_systems(Update, animate_movable_sprite_sheet)
         .add_systems(Update, jump_controllable_characters)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
     // Camera
-    commands.spawn(Camera2dBundle {
+    let mut camera = Camera2dBundle {
         camera_2d: Camera2d {
             clear_color: ClearColorConfig::Custom(Color::rgb(0.08, 0.35, 0.12)),
         },
         ..default()
-    });
+    };
+    camera.projection.scaling_mode = ScalingMode::AutoMin {
+        min_width: 360.0,
+        min_height: 360.0,
+    };
+    commands.spawn(camera);
 
     // Main character
-    commands.spawn((
-        SpriteBundle {
-            sprite: Sprite {
-                custom_size: Some(Vec2::new(150.0, 150.0)),
-                ..default()
-            },
-            texture: asset_server.load("boy.png"),
-            ..default()
-        },
+    let standing_texture_atlas = texture_atlases.add(TextureAtlas::from_grid(
+        asset_server.load("blue_dummy_standing.png"),
+        Vec2::new(32.0, 48.0),
+        40,
+        1,
+        None,
+        None,
+    ));
+    let player = (
         Movable {
-            walk_speed: 300.0,
+            walk_speed: 100.0,
+            standing_texture_atlas: standing_texture_atlas.clone(),
+            walking_texture_atlas: texture_atlases.add(TextureAtlas::from_grid(
+                asset_server.load("blue_dummy_walking.png"),
+                Vec2::new(32.0, 48.0),
+                40,
+                1,
+                None,
+                None,
+            )),
             ..default()
         },
         Jumpable {
-            jump_duration: 0.5,
+            jump_duration: 0.35,
             jump_started: None,
         },
-        Controllable { controlling: true },
-    ));
+        Controllable {
+            controlling: true,
+            timer_to_control: Timer::from_seconds(0.1, TimerMode::Repeating),
+        },
+        Animated {
+            timer_to_animate: Timer::from_seconds(0.07, TimerMode::Repeating),
+            current_frame: 0,
+            number_of_frames: 8,
+        },
+        SpatialBundle::default(),
+    );
+    commands.spawn(player).with_children(|child_builder| {
+        child_builder.spawn(SpriteSheetBundle {
+            texture_atlas: standing_texture_atlas,
+            ..default()
+        });
+    });
 }
 
 fn move_movable_components(
@@ -80,31 +119,84 @@ fn move_movable_components(
 }
 
 fn move_controllable_characters(
-    mut movables: Query<(&mut Movable, &Controllable)>,
+    mut movables: Query<(&mut Movable, &mut Controllable)>,
     input: Res<Input<KeyCode>>,
+    time: Res<Time>,
 ) {
-    movables.iter_mut().for_each(|(mut movable, controllable)| {
-        if controllable.controlling {
-            let mut direction = Vec2::new(0.0, 0.0);
-
-            if input.pressed(KeyCode::W) {
-                direction.y += 1.0;
-            }
-            if input.pressed(KeyCode::A) {
-                direction.x -= 1.0;
-            }
-            if input.pressed(KeyCode::S) {
-                direction.y -= 1.0;
-            }
-            if input.pressed(KeyCode::D) {
-                direction.x += 1.0;
+    movables
+        .iter_mut()
+        .for_each(|(mut movable, mut controllable)| {
+            controllable.timer_to_control.tick(time.delta());
+            if !controllable.timer_to_control.just_finished() {
+                return;
             }
 
-            let direction = direction.normalize_or_zero();
-            movable.current_speed.x = movable.walk_speed * direction.x;
-            movable.current_speed.y = movable.walk_speed * direction.y;
+            if controllable.controlling {
+                let mut direction = Vec2::new(0.0, 0.0);
+
+                if input.pressed(KeyCode::W) {
+                    direction.y += 1.0;
+                }
+                if input.pressed(KeyCode::A) {
+                    direction.x -= 1.0;
+                }
+                if input.pressed(KeyCode::S) {
+                    direction.y -= 1.0;
+                }
+                if input.pressed(KeyCode::D) {
+                    direction.x += 1.0;
+                }
+
+                let direction_vector = direction.normalize_or_zero();
+                movable.current_speed.x = movable.walk_speed * direction_vector.x;
+                movable.current_speed.y = movable.walk_speed * direction_vector.y;
+
+                if direction_vector.x != 0.0 || direction_vector.y != 0.0 {
+                    movable.direction = direction_vector.into();
+                }
+            }
+        })
+}
+
+fn animate_movable_sprite_sheet(
+    mut movables: Query<(&mut Animated, &Movable)>,
+    mut sprites: Query<(&mut Handle<TextureAtlas>, &mut TextureAtlasSprite)>,
+    time: Res<Time>,
+) {
+    movables.iter_mut().for_each(|(mut animated, movable)| {
+        animated.timer_to_animate.tick(time.delta());
+        if !animated.timer_to_animate.just_finished() {
+            return;
         }
-    })
+
+        if animated.current_frame + 1 == animated.number_of_frames {
+            animated.current_frame = 0;
+        } else {
+            animated.current_frame += 1;
+        }
+        let direction_index = movable.direction.get_sprite_index();
+
+        sprites
+            .iter_mut()
+            .for_each(|(mut atlas_handle, mut sprite)| {
+                if *atlas_handle == movable.standing_texture_atlas
+                    && movable.current_speed.length() > 0.0
+                {
+                    *atlas_handle = movable.walking_texture_atlas.clone()
+                } else if *atlas_handle == movable.walking_texture_atlas
+                    && movable.current_speed.length() == 0.0
+                {
+                    *atlas_handle = movable.standing_texture_atlas.clone()
+                }
+                sprite.index = animated.number_of_frames * direction_index + animated.current_frame;
+                sprite.flip_x = match movable.direction {
+                    movable::Direction::NW => true,
+                    movable::Direction::W => true,
+                    movable::Direction::SW => true,
+                    _ => false,
+                }
+            });
+    });
 }
 
 fn jump_controllable_characters(
--- src/movable.rs
+++ src/movable.rs
@@ -1,10 +1,74 @@
 use bevy::{prelude::*, time::Stopwatch};
 
+pub enum Direction {
+    S,
+    SE,
+    E,
+    NE,
+    N,
+    NW,
+    W,
+    SW,
+}
+
+impl Default for Direction {
+    fn default() -> Self {
+        Self::S
+    }
+}
+
+impl From<Vec2> for Direction {
+    fn from(value: Vec2) -> Self {
+        if value.x == 0.0 && value.y < 0.0 {
+            crate::movable::Direction::S
+        } else if value.x > 0.0 && value.y < 0.0 {
+            crate::movable::Direction::SE
+        } else if value.x > 0.0 && value.y == 0.0 {
+            crate::movable::Direction::E
+        } else if value.x > 0.0 && value.y > 0.0 {
+            crate::movable::Direction::NE
+        } else if value.x == 0.0 && value.y > 0.0 {
+            crate::movable::Direction::N
+        } else if value.x < 0.0 && value.y > 0.0 {
+            crate::movable::Direction::NW
+        } else if value.x < 0.0 && value.y == 0.0 {
+            crate::movable::Direction::W
+        } else {
+            crate::movable::Direction::SW
+        }
+    }
+}
+
+impl Direction {
+    pub fn get_sprite_index(&self) -> usize {
+        match &self {
+            Direction::S => 0,
+            Direction::SE => 1,
+            Direction::E => 2,
+            Direction::NE => 3,
+            Direction::N => 4,
+            Direction::NW => 3,
+            Direction::W => 2,
+            Direction::SW => 1,
+        }
+    }
+}
+
 #[derive(Component, Default)]
 pub struct Movable {
     pub walk_speed: f32,
     pub current_speed: Vec2,
     pub current_position: Vec2,
+    pub direction: Direction,
+    pub standing_texture_atlas: Handle<TextureAtlas>,
+    pub walking_texture_atlas: Handle<TextureAtlas>,
+}
+
+#[derive(Component, Default)]
+pub struct Animated {
+    pub timer_to_animate: Timer,
+    pub current_frame: usize,
+    pub number_of_frames: usize,
 }
 
 #[derive(Component, Default)]
@@ -13,7 +77,8 @@ pub struct Jumpable {
     pub jump_started: Option<Stopwatch>,
 }
 
-#[derive(Component)]
+#[derive(Component, Default)]
 pub struct Controllable {
     pub controlling: bool,
+    pub timer_to_control: Timer,
 }
